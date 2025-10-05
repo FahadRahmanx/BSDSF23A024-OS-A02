29ed25a418f7f660197270dc6edca8d0a9e26765
FahadRahmanx <fahad.rahman119@gmail.com>
2025-10-05 22:37:45 +0500

    Implemented Long Listing Logic

--- src/lsv1.0.0.c
+++ src/lsv1.0.0.c
@@ -7,6 +7,7 @@
 *       % lsv1.0.0  /home
 *       $ lsv1.0.0  /home/kali/   /etc/
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,26 +15,55 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 
 extern int errno;
 
 void do_ls(const char *dir);
+void do_ls_long(const char *dir);
+void print_permissions(mode_t mode);
 
 int main(int argc, char const *argv[])
 {
-    if (argc == 1)
+    int opt;
+    int long_format = 0;
+
+    while ((opt = getopt(argc, (char * const *)argv, "l")) != -1)
     {
-        do_ls(".");
+        switch (opt)
+        {
+            case 'l':
+                long_format = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-l] [directory...]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    // If no directory arguments, default to current directory
+    if (optind == argc)
+    {
+        if (long_format)
+            do_ls_long(".");
+        else
+            do_ls(".");
     }
     else
     {
-        for (int i = 1; i < argc; i++)
+        for (int i = optind; i < argc; i++)
         {
             printf("Directory listing of %s : \n", argv[i]);
-            do_ls(argv[i]);
-	    puts("");
+            if (long_format)
+                do_ls_long(argv[i]);
+            else
+                do_ls(argv[i]);
+            puts("");
         }
     }
+
     return 0;
 }
 
@@ -60,4 +90,107 @@ void do_ls(const char *dir)
     }
 
     closedir(dp);
+}
+
+void do_ls_long(const char *dir)
+{
+    struct dirent *entry;
+    DIR *dp = opendir(dir);
+    if (dp == NULL)
+    {
+        fprintf(stderr, "Cannot open directory : %s\n", dir);
+        return;
+    }
+
+    long total_blocks = 0;
+    errno = 0;
+    while ((entry = readdir(dp)) != NULL)
+    {
+        if (entry->d_name[0] == '.')
+            continue;
+
+        char path[1024];
+        snprintf(path, sizeof(path), "%s/%s", dir, entry->d_name);
+
+        struct stat st;
+        if (stat(path, &st) == -1)
+            continue;
+
+        total_blocks += st.st_blocks;
+    }
+
+    if (errno != 0)
+    {
+        perror("readdir failed");
+    }
+
+    rewinddir(dp);
+
+    printf("total %ld\n", total_blocks / 2);
+
+    errno = 0;
+    while ((entry = readdir(dp)) != NULL)
+    {
+        if (entry->d_name[0] == '.')
+            continue;
+
+        char path[1024];
+        snprintf(path, sizeof(path), "%s/%s", dir, entry->d_name);
+
+        struct stat st;
+        if (stat(path, &st) == -1)
+        {
+            perror("stat failed");
+            continue;
+        }
+
+        print_permissions(st.st_mode);
+
+        printf(" %3ld ", (long)st.st_nlink);
+
+        struct passwd *pw = getpwuid(st.st_uid);
+        struct group  *gr = getgrgid(st.st_gid);
+
+        printf("%-8s %-8s ", 
+            pw ? pw->pw_name : "unknown", 
+            gr ? gr->gr_name : "unknown");
+
+        printf("%8ld ", (long)st.st_size);
+
+        char timebuf[64];
+        struct tm *tm_info = localtime(&st.st_mtime);
+        strftime(timebuf, sizeof(timebuf), "%b %d %H:%M", tm_info);
+        printf("%s ", timebuf);
+
+        printf("%s\n", entry->d_name);
+    }
+
+    if (errno != 0)
+    {
+        perror("readdir failed");
+    }
+
+    closedir(dp);}
+
+void print_permissions(mode_t mode)
+{
+    // File type
+    if (S_ISDIR(mode))  printf("d");
+    else if (S_ISLNK(mode)) printf("l");
+    else printf("-");
+
+    // Owner permissions
+    printf((mode & S_IRUSR) ? "r" : "-");
+    printf((mode & S_IWUSR) ? "w" : "-");
+    printf((mode & S_IXUSR) ? "x" : "-");
+
+    // Group permissions
+    printf((mode & S_IRGRP) ? "r" : "-");
+    printf((mode & S_IWGRP) ? "w" : "-");
+    printf((mode & S_IXGRP) ? "x" : "-");
+
+    // Others permissions
+    printf((mode & S_IROTH) ? "r" : "-");
+    printf((mode & S_IWOTH) ? "w" : "-");
+    printf((mode & S_IXOTH) ? "x" : "-");
 }
